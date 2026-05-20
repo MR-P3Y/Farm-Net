44e6f21ce1875d9b1e0b31c6d755b08d0ee762a2
PEYMOONnet <126670278+MR-P3Y@users.noreply.github.com>
2026-05-20 21:57:25 +0330

    feat(media): add media file database model

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -11,6 +11,7 @@ from app.modules.profiles import models as profile_models  # noqa: F401
 from app.modules.stores import models as store_models  # noqa: F401
 from app.modules.products import models as product_models  # noqa: F401
 from app.modules.orders import models as order_models  # noqa: F401
+from app.modules.media import models as media_models  # noqa: F401
 
 
 config = context.config
